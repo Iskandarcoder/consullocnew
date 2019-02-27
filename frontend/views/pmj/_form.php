054770f4d994d813c4f515e5e2c89bd141a8f5f0
AN2828 <47846248+AN2828@users.noreply.github.com>
2019-02-27 18:22:40 +0500

    Update _form.php
    pmj xato _form.php

--- frontend/views/pmj/_form.php
+++ frontend/views/pmj/_form.php
@@ -4,8 +4,6 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use kartik\file\FileInput;
 use kartik\date\DatePicker;
-<<<<<<< HEAD
-=======
 use backend\models\SpNation;
 use yii\helpers\ArrayHelper;
 use backend\models\SpCountry;
@@ -13,203 +11,11 @@ use backend\models\SpDivision;
 use wbraganca\dynamicform\DynamicFormWidget;
 use kartik\select2\Select2;
 
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Pmj */
 /* @var $form yii\widgets\ActiveForm */
 ?>
-<<<<<<< HEAD
-
-<div class="container well well-lg" style="background-color: white;border-radius: 8px!important; color: #18b19c;">
-    <h3><b><?= Yii::t('app', 'Forma №30'); ?></b></h3>
-
-    <div class="board">
-        <ul class="nav nav-tabs">
-            <div class="liner"></div>
-            <li rel-index="0"  class="active">
-                <a  id="s1" href="#step-1" class="btn media-links" aria-controls="step-1" role="tab" data-toggle="tab">
-                  <span><i class="fa fa-user media-imgs"></i></span>
-                </a>
-            </li>
-            <li rel-index="1">
-                <a  id="s2" href="#step-2" class="btn disabled" aria-controls="step-2" role="tab" data-toggle="tab">
-                  <span><i class="fa fa-vcard-o"></i></span>
-                </a>
-            </li>
-            <li  rel-index="2">
-                <a  id="s3" href="#step-3" class="btn disabled" aria-controls="step-3" role="tab" data-toggle="tab">
-                  <span><i class="fa fa-home"></i></span>
-                </a>
-            </li>
-            <li rel-index="3">
-                <a  id="s4" href="#step-4" class="btn disabled" aria-controls="step-4" role="tab" data-toggle="tab">
-                  <span><i class="fa fa-file-text-o"></i></span>
-                </a>
-            </li>
-        </ul>
-    </div>
-    </br>
-    </br>
-
-
-    <div class="pmj-form">
-        <?php $form = ActiveForm::begin(); ?>
-            <div class="tab-content">
-                <div  class="tab-pane active" id="step-1">
-                  <h3 class="step-txt1"><?= Yii::t('app', 'Asosiy ma\'lumotlar'); ?></h3>
-                  <div class="row">
-                    <div class="form-group col-sm-6">
-                      <?= $form->field($model, 'surname')->textInput(['maxlength' => true]) ?>
-                    </div>
-                    <div class="form-group col-sm-6">
-                      <?= $form->field($model, 'pre_surname')->textInput(['maxlength' => true]) ?>
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">
-                      <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-                    </div>
-                    <div class="form-group col-sm-6">
-                      <?= $form->field($model, 'pre_name')->textInput(['maxlength' => true]) ?>
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">
-                      <?= $form->field($model, 'mname')->textInput(['maxlength' => true]) ?>
-                    </div>
-                    <div class="form-group col-sm-6">
-                      <?= $form->field($model, 'pre_mname')->textInput(['maxlength' => true]) ?>
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">
-                      <?= $form->field($model, 'birth_date')
-                        ->widget(DatePicker::classname(), [
-                            'language' => 'ru',
-                            'pluginOptions' => [
-                                'format' => 'yyyy-mm-dd',
-                                'autoclose'=>true,
-                                'todayHighlight' => true
-                            ],
-                        ]);
-                      ?>
-                    </div>
-                    <div class="form-group col-sm-6">
-                      <?= $form->field($model, 'birth_place')->textInput(['maxlength' => true]) ?>
-                    </div>
-                  </div>
-
-                  <div class="row">
-                    <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">
-                      <?= $form->field($model, 'nationality_id')->textInput() ?>
-                    </div>
-                    <div class="form-group col-sm-6">
-                      <?php echo $form->field($model, 'photo')->widget(FileInput::classname(), [
-                        'options' => ['accept' => 'image/*','maxSize'=>'500000'],
-                            ]);
-                      ?>
-                    </div>
-                  </div> 
-                </div>    <!-- 1-step tugadi shu yerda --> 
-                <div  class="tab-pane"  id="step-2"> 
-                  <h3 class="step-txt2"><?= Yii::t('app', 'Pasport'); ?></h3> 
-                    <div class="row">
-                      <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">                    
-                        <?= $form->field($model, 'seria_pasp')->textInput(['maxlength' => true]) ?>
-                      </div>
-                      <div class="form-group col-sm-6">
-                        <?= $form->field($model, 'numb_pasp')->textInput() ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">  
-                        <?= $form->field($model, 'pasp_date')->textInput() ?>
-                      </div>
-                      <div class="form-group col-sm-6">
-                        <?= $form->field($model, 'pasp_place')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'">  
-                        <?= $form->field($model, 'living_place')->textInput(['maxlength' => true]) ?>
-                      </div>
-                      <div class="form-group col-sm-6">
-                        <?= $form->field($model, 'tel')->textInput() ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-6 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'citizenship_id')->textInput() ?>
-                      </div>
-                      <div class="form-group col-sm-6">
-                        <?= $form->field($model, 'second_cityzenship_id')->textInput() ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-12 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'reason')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-12 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'security')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-12 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'law_court')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-12 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'criminal')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="form-group col-sm-12 data-toggle='tooltip' data-placement='top' title='Faqat lotin harflarida!'"> 
-                        <?= $form->field($model, 'army')->textInput(['maxlength' => true]) ?>
-                      </div>
-                    </div>
-                        
-                </div> <!-- 2-step tugadi -->
-                <div  class="tab-pane"  id="step-3">
-                    <h3 class="step-txt3"><?= Yii::t('app', 'O`zbekistonda yashagan joyi'); ?></h3>
-                        
-                        
-
-                        
-                </div> <!-- 3-step tugadi -->
-                <div  class="tab-pane"  id="step-4">
-                    <h3 class="step-txt4"><?= Yii::t('app', 'Qo\'shimcha ma\'lumotlar' ); ?></h3>
-                        
-                </div>
-                <div class="row">
-                    <div class="col-sm-4">
-                       <button id="back" class="btn btn-primary btn-lg" type="button" ><i class="fa fa-chevron-left"></i> <?= Yii::t('app', 'Orqaga'); ?></button>
-                    </div>
-                    <div class="col-sm-offset-4 col-sm-4">
-                        <button id="next" class="btn btn-primary btn-lg" type="button" ><?= Yii::t('app', 'Keyingi'); ?> <i class="fa fa-chevron-right"></i></button>
-                        <div id="send" class="form-group">
-                          <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        <?php ActiveForm::end(); ?>
-    </div>
-=======
 <style type="text/css">
   .fileinput-cancel-button {
     display: none;
@@ -648,21 +454,10 @@ use kartik\select2\Select2;
       </div>
     </div>
   <?php ActiveForm::end(); ?>  
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
 </div>
 
 <?php
 $this->registerJs("$(document).ready(function($){
-<<<<<<< HEAD
-    step=1; 
-    $('.btn-success').hide();
-    $('#send').hide();
-    $('#back').hide();
-    $('#kus-wed_name').parents('.form-group').hide();
-    $('#next').click(function(){            
-  var n=step;
-     if(step<4)
-=======
 
   
   $('#pmj-numb_pasp').mask('0000000');
@@ -674,7 +469,6 @@ $this->registerJs("$(document).ready(function($){
     $('#next').click(function(){            
   var n=step;
      if(step<7)
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
          {
 
   n++;
@@ -696,19 +490,6 @@ $this->registerJs("$(document).ready(function($){
     $('.step-txt').removeClass('text-disable');
         }
       });
-<<<<<<< HEAD
-//////////////////////////////////////////////////////////////////////////////////
-$('#kus-marital_status_id').change(function()
-  {
-    if(($(this).val()== 2)||($(this).val()== 6))
-      $('#kus-wed_name').parents('.form-group').show();
-    else
-      $('#kus-wed_name').parents('.form-group').hide();
-    });
-
-
-=======
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
 //////////////////////////////////////////////////////////////////////////////////      
  $('.nav-tabs > li > a').click(function() { 
             if($(this).hasClass('disabled')) {
@@ -718,28 +499,15 @@ $('#kus-marital_status_id').change(function()
             $('.nav-tabs > li').each(function(index, item) {  
                 $(item).attr('rel-index', index - linkIndex+1);
                                 });
-<<<<<<< HEAD
-       if(linkIndex==4)
-     {
-     $('#next').hide();
-     $('#send').show();
-     $('.btn-success').show();
-=======
        if(linkIndex==7)
      {
      $('#next').hide();
      $('#send').show();     
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
    }
    else
      {
      $('#next').show(); 
-<<<<<<< HEAD
-     $('#send').hide();
-     $('.btn-success').hide();
-=======
      $('#send').hide();    
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
      }  
 
       if(linkIndex>1)
@@ -754,13 +522,6 @@ $('#kus-marital_status_id').change(function()
      step= linkIndex; 
             }
 });
-<<<<<<< HEAD
-
- 
-
-
-=======
->>>>>>> aa4b3562e521a8ba62c133b5d1d8a9ab9a50952b
 })");
 
 ?> 
